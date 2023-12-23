2986f9fef53834068313932bcc29b275e17b6bf9
TheSoundOfWA <121793003+TheSoundOfWA@users.noreply.github.com>
2023-12-23 17:40:39 +0800

    Update A.cpp

--- code/A.cpp
+++ code/A.cpp
@@ -1,11 +1,14 @@
 // User Name: JonathanLiu
-// Link: https://atcoder.jp/contests/abc___/tasks/abc____a
+// Link: https://atcoder.jp/contests/abc333/tasks/abc333_a
 
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
+  int n;
+  scanf("%d", &n);
   
+  for (int i = 1; i <= n; i++) printf("%d", n);
   
   return 0;
 }
